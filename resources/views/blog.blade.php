2f2310f0a32f9c56d6181c77b0d26cfb78fc509d
Div-VT <div@voicetube.com>
2017-11-03 15:44:44 +0800

    blog test

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,401 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Excel To HTML using codebeautify.org</title>
+</head>
+
+<body>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Excel To HTML using codebeautify.org</title>
+    </head>
+
+    <body>
+        <link rel="stylesheet" href="http://tw.blog.voicetube.com/wp-content/themes/magnovus/style.css" />n
+
+
+        <div style="padding:6px;font-size: 2rem">
+            <p style="text-align: center;">
+                <span style="color: #ff6600;">
+                    <span style="font-weight: 400;">想得到老闆的賞識？
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">想在工作上得到重視？
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">拍馬屁是你必學的藝術
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">現在就教你
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">如何拍得精準不過度！</span>
+                </span>
+            </p>
+            <p>
+                <img class="wp-image-60902 size-full aligncenter" src="https://tw.blog.voicetube.com/wp-content/uploads/2017/10/86c478e1-flattery1.jpg"
+                    alt="" width="95%" />
+            </p>
+            <p>
+                <!--more-->
+            </p>
+            <h2>
+                <span style="font-weight: 400;">「拍馬屁」的用法</span>
+            </h2>
+            <p>
+                <span style="font-weight: 400;">瞭解「拍馬屁」的藝術前，我們先來學學「拍馬屁」的英文要怎麼說吧！</span>
+            </p>
+            <h3>
+                <b>brown-noser/brown-nose
+
+
+                    <br />
+                </b>
+            </h3>
+            <p>
+                <img class="aligncenter wp-image-60906 size-full" src="https://tw.blog.voicetube.com/wp-content/uploads/2017/10/cde17de6-nose.jpg"
+                    alt="" width="95%" />
+            </p>
+            <p>
+                <span style="font-weight: 400;">這個字的解釋其實是比較具有貶意的，它是指馬屁精的行為，就像是把鼻子放在想拍馬屁對象的屁股位置，因此鼻子才會沾到穢物而變成咖啡色。</span>
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">The</span>
+                    <b> brown-noser</b>
+                    <span style="font-weight: 400;"> bought a bottle of wine for his boss.
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">那個馬屁精買了一瓶酒給他的老闆。</span>
+                </p>
+            </blockquote>
+            <h3>
+                <b>apple polisher</b>
+            </h3>
+            <p>
+                <img class="aligncenter wp-image-60903 size-full" src="https://tw.blog.voicetube.com/wp-content/uploads/2017/10/c3d1e6f8-applepolisher.jpg"
+                    alt="" width="95%" />
+            </p>
+            <p>
+                <span style="font-weight: 400;">把蘋果擦亮和拍馬屁有什麼關係呢？它的由來是因為美國的學生會藉由送蘋果讓老師開心，希望老師可以多多關照自己。</span>
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">My ex-coworker is a typical </span>
+                    <b>apple-polisher</b>
+                    <span style="font-weight: 400;"> because she agrees pretty much everything the boss says.
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">我的前同事是個典型的馬屁精，因為她總是贊同老闆說的每一句話。</span>
+                </p>
+            </blockquote>
+            <h3>
+                <b>the boss&#8217;s pet</b>
+            </h3>
+            <p>
+                <img class="aligncenter wp-image-60907 size-full" src="https://tw.blog.voicetube.com/wp-content/uploads/2017/10/d51555e0-pet.jpg"
+                    alt="" width="95%" />
+            </p>
+            <p>
+                <span style="font-weight: 400;">當一個人表現得對老闆言聽計從，那他的行為就像是老闆養的寵物一樣。</span>
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">If you act like a </span>
+                    <b>boss&#8217;s pet</b>
+                    <span style="font-weight: 400;">, it may affect your friendship with your colleagues.
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">如果你表現得像是個馬屁精，你有可能失去與同事的友誼。</span>
+                </p>
+            </blockquote>
+            <h3>
+                <b>yes-man</b>
+            </h3>
+            <p>
+                <img class="wp-image-60908 size-full aligncenter" src="https://tw.blog.voicetube.com/wp-content/uploads/2017/10/c96af528-yes.jpg"
+                    alt="" width="95%" />
+            </p>
+            <p>
+                <span style="font-weight: 400;">形容一個人對想討好的對象所提出的意見或要求都答應且順從。</span>
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">It is not surprising that our boss is always surrounded by a lot of </span>
+                    <b>yes-men</b>
+                    <span style="font-weight: 400;">.
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">我對於我們老闆身邊總是圍繞著很多馬屁精這件事不感到意外。</span>
+                </p>
+            </blockquote>
+            <h3>
+                <b>kiss up to someone</b>
+            </h3>
+            <p>
+                <img class="aligncenter wp-image-60905 size-full" src="https://tw.blog.voicetube.com/wp-content/uploads/2017/10/2aeb65cb-kissup.jpg"
+                    alt="" width="95%" />
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">He&#8217;s always </span>
+                    <b>kissing up to our boss</b>
+                    <span style="font-weight: 400;">! God! He is just so annoying.
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">他又要去拍我們老闆馬屁了！天啊！他真的很煩人。</span>
+                </p>
+            </blockquote>
+            <h3>
+                <b>butter somebody up</b>
+            </h3>
+            <p>
+                <img class="aligncenter wp-image-60904 size-full" src="https://tw.blog.voicetube.com/wp-content/uploads/2017/10/0d4109bc-butter.jpg"
+                    alt="" width="95%" />
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">The first thing you have to do as a salesman is to </span>
+                    <b>butter the clients up</b>
+                    <span style="font-weight: 400;"> in order to gain their trust.
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">身為業務，為了要拿到案子，你要做的第一件事就是拍客戶馬屁。</span>
+                </p>
+            </blockquote>
+            <h2>
+                <span style="font-weight: 400;">「拍馬屁」藝術：不著痕跡的技巧</span>
+            </h2>
+            <p>
+                <span style="font-weight: 400;">「拍馬屁」是一門藝術，它的重點在於如何抓對時機說對話，因為這樣就可以讓你在同樣的情況下，比別人更具優勢。如果你還對「拍馬屁」有壞印象，趕快看這支影片，你會了解這其實是件有益的事！</span>
+            </p>
+            <p style="text-align: center;">
+                <span style="font-weight: 400;">
+                    <a href="https://tw.voicetube.com/videos/52221?mtc=blog_sunny_60900_20171103" target="_blank" rel="noopener">阿諛諂媚，其實很重要？ (The Importance of Flattery)</a>
+                    <br />
+                    <a href="https://tw.voicetube.com/videos/52221?mtc=blog_sunny_60900_20171103" target="_blank" rel="noopener">
+                        <img class="aligncenter wp-image-60901 size-full" src="https://tw.blog.voicetube.com/wp-content/uploads/2017/10/813039ae-blog-beapro1.jpg"
+                            alt="" width="95%" />
+                    </a>
+                </span>
+            </p>
+            <h3>
+                <span style="font-weight: 400;">1. 把拍馬屁包裝成尋求意見</span>
+            </h3>
+            <p>
+                <span style="font-weight: 400;">當對方主導一個成功的策略時，你可以這樣說：</span>
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">“The marketing campaign was a great success. How were you able to pull off the strategy so successfully?”
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">「這次的行銷活動超成功。你是怎麼成功執行這個策略的？」 </span>
+                </p>
+            </blockquote>
+            <p>
+                <span style="font-weight: 400;">當對方所提的廣告達到不錯成效時，你可以這樣說：</span>
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">“We received a lot of positive feedback about our new commercial recently. How were you able to come
+                        up with such a creative idea?”
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">「我們最近收到很多對新廣告的正向回饋。你是怎麼想出這麼有創意的點子的？」</span>
+                </p>
+            </blockquote>
+            <h3>
+                <span style="font-weight: 400;">2. 暗示你拍馬屁的對象：我要拍你馬屁了！</span>
+            </h3>
+            <p>
+                <span style="font-weight: 400;">當對方換了一個新髮型時，你可以這樣說：</span>
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">“I don&#8217;t want to embarrass you, but you look great with that new hairstyle.”
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">「我知道這樣說會讓你尷尬，但你的新髮型真的很好看。」</span>
+                </p>
+            </blockquote>
+            <p>
+                <span style="font-weight: 400;">當對方有一些特殊表現時，你可以這樣說：</span>
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">“You are going to hate me for saying this, but you really are a great asset to our company.”  </span>
+                    <span style="font-weight: 400;">「我知道你可能不會想聽到這個，但你真的是本公司最棒的資產。」</span>
+                </p>
+                <p>
+                    <span style="font-weight: 400;">“I know you won&#8217;t want me to say this, but your outstanding performance has always been a great
+                        example for us to follow.”
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">「我知道你不希望我這麼說，但是你的優秀表現一直是我們仿效的模範。」</span>
+                </p>
+            </blockquote>
+            <h3>
+                <span style="font-weight: 400;">3. 對奉承對象周遭的朋友讚美他</span>
+            </h3>
+            <p>
+                <span style="font-weight: 400;">有些人可能會不喜歡別人直接讚美他，但如果你跟他的朋友讚美他，當這個讚美傳到他耳裡，讚美就顯得更誠心。</span>
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">“I really admire his decision-making and execution abilities.”
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">「我非常欽佩他的決策和執行能力。」</span>
+                </p>
+            </blockquote>
+            <h3>
+                <span style="font-weight: 400;">4. 不要馬上說 “Yes” 或 “I agree” </span>
+            </h3>
+            <p>
+                <span style="font-weight: 400;">當你想奉承的對象提出任何意見或想法時，如果你想都不想的在第一時間就附和或者稱讚他，對方很容易察覺你只是想對他拍馬屁。因此在這種情況下，你應該要先提出自己的建議 (不是用詞尖銳的建議)，然後再附和或稱讚對方的想法，這樣就顯得真誠多了！</span>
+            </p>
+            <blockquote>
+                <p>
+                    <span style="font-weight: 400;">Supervisor：What do you think of the project I presented?
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">Subordinate：I think it’s a good idea, but it won’t work unless you have accurate data. What sources did
+                        you use?
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">Supervisor：It’s all World Bank and United Nations data—it’s all super accurate.
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">Subordinate：Wow! That’s great! I’m excited to see how it turns out.</span>
+                </p>
+                <p>
+                    <span style="font-weight: 400;">主管：你覺得我剛剛報告的專案計畫如何？
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">部屬：我覺得是個不錯的想法，但是必須要有精確的數據才能執行。你採用的資料來源是什麼？
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">主管：全部的數據都是世界銀行和聯合國的資料，都是非常精確的數據。
+
+
+                        <br />
+                    </span>
+                    <span style="font-weight: 400;">部屬：哇！這樣超棒的！我很期待看到成果。</span>
+                </p>
+            </blockquote>
+            <p>看完這篇文章，你是不是對拍馬屁這件事改觀了呢？拍馬屁其實就是懂得在對的時間說好聽話，這不僅讓聽的人開心，也讓說的人得到想要獲得的結果，簡直就是雙贏的局面啊！</p>
+            <h2>
+                <span style="font-weight: 400;">想要瞭解更多有趣的英文用法</span>
+            </h2>
+            <h2>
+                <span style="font-weight: 400;">VoiceTube App 網羅各種有趣影片，讓英文學習變得更有趣</span>
+            </h2>
+            <p>
+                <span style="font-weight: 400;">你總是靠唸文法書或死背單字來學英文嗎？學習英文不該是一件枯燥乏味的事！
+
+
+                    <a href="http://hyperurl.co/m3rc91" target="_blank" rel="noopener">VoiceTube App</a> 暸解各位在學習英文上的問題，所以我們搜羅了很多有趣而且又可以學到道地英文的影片，希望藉由更融入生活的影片來幫助各位練就一口流利英文。所以你還在猶豫什麼？趕快下載
+
+
+                    <a href="http://hyperurl.co/m3rc91" target="_blank" rel="noopener">VoiceTube App</a>，讓你重燃英文學習的熱情。
+
+                </span>
+            </p>
+            <p>
+                <strong>▶ Android: </strong>
+                <a href="https://goo.gl/pmC5JG" target="_blank" rel="noopener">https://goo.gl/pmC5JG</a>
+            </p>
+            <p>▶
+
+
+                <strong>iOS: </strong>
+                <a href="https://goo.gl/PJqKKp" target="_blank" rel="noopener">https://goo.gl/PJqKKp</a>
+            </p>
+            <p>
+                <a href="http://hyperurl.co/m3rc91" target="_blank" rel="noopener">
+                    <img class="aligncenter wp-image-60280 size-full" src="https://tw.blog.voicetube.com/wp-content/uploads/2016/06/7e6cd6b7-nostalgia-blogapp.jpg"
+                        alt="" width="95%" />
+                </a>
+            </p>
+            <p>
+                <span style="font-weight: 400;">文/ Sunny Yang
+
+
+                    <br />
+                </span>
+                <span style="font-weight: 400;">圖/ Pexel
+
+
+                    <br /> 參考資料/
+
+                </span>
+                <a href="https://www.linkedin.com/pulse/20131105150120-69244073-7-sneaky-influence-tactics-you-never-saw-coming/?trk=tod-posts-post1-ptlt"
+                    target="_blank" rel="noopener">The Sneaky Influence Tactics You Never Saw Coming</a>
+                <br />
+                <span style="font-weight: 400;">作者簡介/  </span>
+                <a href="https://tw.voicetube.com/?ref=sign">
+                    <span style="font-weight: 400;">VoiceTube看影片學英語
+
+
+                        <br />
+                    </span>
+                </a>
+                <span style="font-weight: 400;">學英文不是艱難的任務，而是一種融入生活的自然態度。今天起，丟掉厚重的教科書，每日鎖定 </span>
+                <a href="https://tw.blog.voicetube.com/">
+                    <span style="font-weight: 400;">VoiceTube看影片學英語官方部落格</span>
+                </a>
+                <span style="font-weight: 400;">，給自己一個流利『用』英語的機會！</span>
+            </p>
+        </div>
+    </body>
+
+    </html>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blogtest', function () {
+    return view('blog');
+});
