2e0ed3080f193b344a4d831bad48af5537dafeb3
Div-VT <div@voicetube.com>
2017-11-05 16:46:58 +0800

    fixed get request data

--- app/Http/Controllers/Mailchimp/WebhookController.php
+++ app/Http/Controllers/Mailchimp/WebhookController.php
@@ -16,20 +16,20 @@ class WebhookController extends Controller
         $data = $request->all();
         $mailchimp = new MailchimpWebhook;
 
-        $mailchimp->type = $data->type;
-        $mailchimp->fired_at = $data->fired_at;
-        $mailchimp->mailchimp_id = $data->data->id;
-        $mailchimp->email = $data->data->email;
-        $mailchimp->email_type = $data->data->email_type;
-        $mailchimp->ip_opt = $data->data->ip_opt;
-        $mailchimp->web_id = $data->data->web_id;
-        $mailchimp->list_id = $data->data->list_id;
-        $mailchimp->merges_email = $data->data->merges->EMAIL;
-        $mailchimp->merges_interests = $data->data->merges->INTEREST;
-        $mailchimp->merges_purpose = $data->data->merges->PURPOSE;
-        $mailchimp->merges_url = $data->data->merges->URL;
+        $mailchimp->type = $data['type'];
+        $mailchimp->fired_at = $data['fired_at'];
+        $mailchimp->mailchimp_id = $data['data']['id'];
+        $mailchimp->email = $data['data']['email'];
+        $mailchimp->email_type = $data['data']['email_type'];
+        $mailchimp->ip_opt = $data['data']['ip_opt'];
+        $mailchimp->web_id = $data['data']['web_id'];
+        $mailchimp->list_id = $data['data']['list_id'];
+        $mailchimp->merges_email = $data['data']['merges']['EMAIL'];
+        $mailchimp->merges_interests = $data['data']['merges']['INTEREST'];
+        $mailchimp->merges_purpose = $data['data']['merges']['PURPOSE'];
+        $mailchimp->merges_url = $data['data']['merges']['URL'];
 
-        $mailchimpType = $data->type;
+        $mailchimpType = $data['type'];
 
         if ($mailchimpType == self::MAILCHIMP_TYPE_SUBSCRIPTION) {
             $mailchimp->ip_signup = $data->data->ip_signup;
